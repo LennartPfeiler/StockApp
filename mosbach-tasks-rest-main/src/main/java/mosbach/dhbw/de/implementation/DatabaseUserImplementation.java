17e9c2560f7fe37214bef0847202ca5f6f49a525
Pfeiler, Lennart <lennart.pfeiler@tecalliance.net>
2024-09-16 12:53:21 +0200

    Began Backend

---
+++ mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/implementation/DatabaseUserImplementation.java
@@ -0,0 +1,55 @@
+﻿package mosbach.dhbw.de.implementation;
+
+import mosbach.dhbw.de.model.User;
+
+public class DatabaseUserImplementation{
+
+    static DatabaseUserImplementation databaseUser = null;
+
+    private DatabaseUserImplementation(){
+        
+    }
+
+    static public DatabaseUserImplementation getDatabaseUser() {
+        if (databaseUser == null)
+            databaseUser = new DatabaseUserImplementation();
+        return databaseUser;
+    }
+
+    public User getUserProfile(int userID){
+        User user = null;
+        string getUser_database_query= "SELECT * FROM User WHERE UserID = " + userID + ";";
+        //get from database
+        return user;
+    }
+
+    public int addUser(User user) {
+        int userID = 0;
+        // write the data into database
+        String addUser_database_query = "Insert into User (FirstName, LastName, EMail, Password) Values ('" + user.getFirstName() + "', '" + user.getLastName() + "', '" + user.getEmail() + "', '" + user.getPhone() +"', '" + user.getPassword() + "');";
+        //Write into database
+        return userID;
+    }
+
+    public boolean editUser(User user) {
+        boolean edited;
+        // change the data of user, found by id
+        String editUser_database_query = "UPDATE Users" +
+                "       SET FirstName = '" + user.getFirstName() + "', LastName = '" + user.getLastName() +
+                        "', EMail = '" + user.getEmail() + "', Password = '" + user.getPassword() + "'" +
+                "       WHERE UserID = " + user.getUserID() + ";";
+        //Write into database
+        return edited;
+    }
+
+    @Override
+    public boolean deleteUser(int userID) {
+        boolean deleted;
+        // sql strings to delete the user and the connections, if he uses carpools (foreign key)
+        String deleteUser_database_query = "DELETE FROM Users" +
+                "       WHERE UserID = " + userID + ";";
+        //Delete from database
+        return deleted;
+    }
+
+}
---
+++ mosbach-tasks-web-main/public/js/StockApp.js
@@ -0,0 +1,73 @@
+﻿///* Login *//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let counter = 1;
+function login(profileSchema){
+    setCookie("userID", profileSchema.userID.value);
+    let password = profileSchema.password.value;
+    if(counter == 1){
+        counter += 1;
+        getProfile();
+    }
+    if(password != getCookie("password"))
+        alert("Das Passwort ist falsch versuche es erneut!");
+    else{
+        counter = 1;
+        document.location = "index.html";
+    }
+}
+
+///*Cookies*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Getting cookie
+function getCookie(cookieName){
+    // source W3Schools
+    let name = cookieName + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(const element of ca) {
+        let c = element;
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+//Setting cookie
+function setCookie(cookieName, cookieValue) {
+    document.cookie = cookieName+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; SameSite=Strict";
+    document.cookie = cookieName + "=" + cookieValue + "; SameSite=Strict ; path=/";
+}
+
+//Testing if user cookie is there
+function testCookie(){
+    if(getCookie("userID") != "" && getCookie("userID") != null){}
+    else{
+        document.location = "login.html";
+    }
+}
+
+// GET USER ID FROM COOKIE --------------------------------------------------------------------------------------------------------------------------------------------------------
+//getting Profile for user of the Website
+function getProfile(){
+    const settingsGetProfile = {
+        "async": false,
+        "url": "https://sharearide-carpool.herokuapp.com/api/v1.0/user?userID="+ getCookie("userID"),
+        "method": "GET",
+        "headers": {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+    $.ajax(settingsGetProfile).done(function (user) {
+        setCookie("userID", user.userID);
+        setCookie("firstName", user.firstName);
+        setCookie("lastName", user.lastName);
+        setCookie("eMail", user.eMail);
+        setCookie("phone", user.phone);
+        setCookie("password", user.password);
+    });
+}
