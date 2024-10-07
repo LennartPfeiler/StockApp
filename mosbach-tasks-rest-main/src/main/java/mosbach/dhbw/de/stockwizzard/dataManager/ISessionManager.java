c0c5ccd15821c5e082fcce157e84b98bba709f1b
Arvid17 <arv.harang.23@lehre.mosbach.dhbw>
2024-10-07 15:25:31 +0200

    Methode im Interface

--- mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/stockwizzard/dataManager/ISessionManager.java
+++ mosbach-tasks-rest-main/src/main/java/mosbach/dhbw/de/stockwizzard/dataManager/ISessionManager.java
@@ -5,4 +5,6 @@ public interface ISessionManager{
     public void createSession(String email, String token);
 
     public void deleteSession(String email, String token);
+
+    public boolean validToken(String token, String email);
 }
