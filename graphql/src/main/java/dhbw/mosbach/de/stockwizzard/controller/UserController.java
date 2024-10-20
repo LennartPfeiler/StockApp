3298ec6609fc868996b1fd92730245eff482804f
Arvid17 <arv.harang.23@lehre.mosbach.dhbw>
2024-10-20 17:13:40 +0200

    GraphQL

--- graphql/src/main/java/dhbw/mosbach/de/stockwizzard/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-@Controller
-public class UserController {
-    @QueryMapping
-    public User UserByEmail(@Argument String email) {
-        return UserService.getUserByEmail(email);
-    }
-
-    @SchemaMapping
-    public 
