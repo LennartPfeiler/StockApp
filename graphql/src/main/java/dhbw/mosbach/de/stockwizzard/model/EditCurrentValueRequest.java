db65a8e83023dc45265f7c512d01d30d10e68d43
Arvid17 <arv.harang.23@lehre.mosbach.dhbw.de>
2024-10-24 16:33:25 +0200

    GraphQL

---
+++ graphql/src/main/java/dhbw/mosbach/de/stockwizzard/model/EditCurrentValueRequest.java
@@ -0,0 +1,97 @@
+package dhbw.mosbach.de.stockwizzard.model;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class EditCurrentValueRequest {
+
+    @JsonProperty("token")
+    private String token;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("symbol")
+    private String symbol;
+    @JsonProperty("newvalue")
+    private Double newValue;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public EditCurrentValueRequest() {
+    }
+
+    public EditCurrentValueRequest(String token, String email, String symbol, Double newValue) {
+        super();
+        this.token = token;
+        this.email = email;
+        this.symbol = symbol;
+        this.newValue = newValue;
+    }
+
+    @JsonProperty("token")
+    public String getToken() {
+        return token;
+    }
+
+    @JsonProperty("token")
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("symbol")
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @JsonProperty("symbol")
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+
+    }
+
+    @JsonProperty("newvalue")
+    public Double getNewValue() {
+        return newValue;
+
+    }
+
+    @JsonProperty("newvalue")
+    public void setNewValue(Double newValue) {
+        this.newValue = newValue;
+
+    
+
+    
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
--- graphql/src/main/java/dhbw/mosbach/de/stockwizzard/service/dataManager/IPortfolioStockManager.java
+++ graphql/src/main/java/dhbw/mosbach/de/stockwizzard/service/dataManager/IPortfolioStockManager.java
@@ -5,17 +5,26 @@ import dhbw.mosbach.de.stockwizzard.model.PortfolioStock;
 import dhbw.mosbach.de.stockwizzard.model.PortfolioStockValue;
 import dhbw.mosbach.de.stockwizzard.model.Transaction;
 
-public interface IPortfolioStockManager{
+public interface IPortfolioStockManager {
 
     public void createPortfolioStockTable();
 
-    public void increasePortfolioStock(Integer portfolioId, String symbol, Double stockAmount, Double totalPrice);
+    public void increasePortfolioStock(Integer portfolioId, String symbol, Double stockAmount, Double totalPrice, String email);
 
     public List<PortfolioStock> getAllPortfolioStocks(String email, String sortby);
 
-    public void decreasePortfolioStock(Integer portfolioId, String symbol, Double stockAmount, Double totalPrice, PortfolioStockValue portfolioStockValues, List<Transaction> transactionsInPortfolio);
+    public void decreasePortfolioStock(Double newCurrentValue, Double newBoughtValue, Double stockAmount, Integer portfolioId, String symbol);
 
     public PortfolioStockValue getPortfolioStockValues(Double sellRequestAmount, String email, String symbol);
 
+    public void deletePortfolioStock(String symbol, Integer portfolioId);
+
     public void deleteAllPortfolioStocks(String email);
+    
+    public PortfolioStock getPortfolioStock(String email, String symbol);
+
+    public void editCurrentValue(String email, String symbol, Double newCurrentValue);
+
+    public void addPortfolioStock(Integer portfolioId, String symbol, Double stockAmount, Double totalPrice);
+
 }
