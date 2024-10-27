479e787c4ec3955d89dad504af6cf57e617075b5
rob011004 <robertsn2004@gmail.com>
2024-10-27 15:06:57 +0100

    html und css in der spa angepasst

--- stockwizzard-spa/src/app/login/login.component.ts
+++ stockwizzard-spa/src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'an-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- stockwizzard-spa/src/app/signup/signup.component.ts
+++ stockwizzard-spa/src/app/signup/signup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'an-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
