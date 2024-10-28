900cf07436b4da2f5049fb94ca3b9b89d4fa1d24
LennartPfeiler <lennart.pfeiler@web.de>
2024-10-28 13:36:02 +0100

    Implement logout check on every page

--- stockwizzard-spa/src/app/profile/profile.component.ts
+++ stockwizzard-spa/src/app/profile/profile.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthComponent } from '../auth/auth.component';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  private authComponent: AuthComponent;
+  constructor(public router: Router) {
+    this.authComponent = new AuthComponent(router);
+  }
 
+  ngOnInit(): void {
+    this.authComponent.disableGoBackFunction();
+  }
 }
