bf5347e5c5458937e6ee247d700b05d5bab9dacf
LennartPfeiler <lennart.pfeiler@web.de>
2024-10-29 02:02:38 +0100

    Add graphQL calls

--- stockwizzard-spa/src/app/portfolio/portfolio.component.ts
+++ stockwizzard-spa/src/app/portfolio/portfolio.component.ts
@@ -16,7 +16,10 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 
 export class PortfolioComponent implements OnInit {
+  budget: string = '';
+  portfolioValue: number | null = null;
   transactions: any[] = [];
+  portfolioStocks: any[] = [];
   loading: boolean = true;
   error: any;
   private authComponent: AuthComponent;
@@ -26,8 +29,62 @@ export class PortfolioComponent implements OnInit {
 
   ngOnInit(): void {
     this.authComponent.disableGoBackFunction();
+    this.displayAllDatabaseData();
+    this.getAllPortfolioStocks();
+  }
+  test(): void{
+    console.log("test");
+  }
+
+  displayAllDatabaseData(){
+    this.getUserBudget();
+    this.getPortfolioValue();
     this.loadTransactions();
   }
+
+  getCurrentDateTime(): string {
+    const now: Date = new Date();
+    const year: number = now.getFullYear(); 
+    const month: string = String(now.getMonth() + 1).padStart(2, '0'); 
+    const day: string = String(now.getDate()).padStart(2, '0'); 
+    const hours: string = String(now.getHours()).padStart(2, '0'); 
+    const minutes: string = String(now.getMinutes()).padStart(2, '0'); 
+    const seconds: string = String(now.getSeconds()).padStart(2, '0'); 
+
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+}
+
+
+  toggleLabel(): void {
+    const label = document.getElementById('quantity-label') as HTMLLabelElement;
+    const button = document.getElementById('toggle-label') as HTMLButtonElement;
+
+    if (label.textContent === 'Quantity in $:') {
+        label.textContent = 'Quantity in Stocks:';
+        button.textContent = 'Switch to $';
+    } else {
+        label.textContent = 'Quantity in $:';
+        button.textContent = 'Switch to Stocks';
+    }
+  }
+
+// Check if all necessary fields for creating an order are filled
+  checkFields(): void {
+    const stockName = (document.getElementById('stock-name') as HTMLInputElement).value.trim();
+    const quantity = (document.getElementById('quantity') as HTMLInputElement).value.trim();
+
+    const buyButton = document.getElementById('buy-stock') as HTMLButtonElement;
+    const sellButton = document.getElementById('sell-stock') as HTMLButtonElement;
+
+    if (stockName !== "" && quantity !== "") {
+        buyButton.disabled = false; 
+        sellButton.disabled = false; 
+    } else {
+        buyButton.disabled = true;
+        sellButton.disabled = true;
+    }
+  }
+
 
   loadTransactions(): void {
     const email = this.authComponent.getCookie("email"); // Hole die E-Mail aus dem Cookie
@@ -43,17 +100,13 @@ export class PortfolioComponent implements OnInit {
       .pipe(
         tap(({ data } : any) => 
           {
-          this.transactions = data.transactions || [];
-          this.displayTransactionHistory(this.transactions)
+          this.transactions = data.getAllTransactions || [];
+          this.displayTransactionHistory(this.transactions);
         }
       )
       )
       .subscribe({
         next: () => {
-          // console.log("AHHHHHHH");
-          // console.log(transactions);
-          // this.transactions = transactions || [];
-          // this.loading = false;
         },
         error: (error) => {
           this.error = error;
@@ -86,8 +139,7 @@ export class PortfolioComponent implements OnInit {
   // }
 
   displayTransactionHistory(transactions: any[]): void {
-    const transactionHistoryContainer = document.querySelector('#transaction-history') as HTMLElement;
-    if (!transactionHistoryContainer) console.log("KLLLLLLLLLLLW");
+    const transactionHistoryContainer = document.querySelector('.transaction-history') as HTMLElement;
 
     transactionHistoryContainer.innerHTML = ''; 
     const heading = document.createElement('h2');
@@ -96,44 +148,205 @@ export class PortfolioComponent implements OnInit {
 
     transactions.forEach(transaction => {
         const transactionDiv = document.createElement('div');
-        const type = transaction.transactionType === 1 ? 'Bought' : 'Sold'; // Ternäre Bedingung
+        const type = transaction.transactionType === 1 ? 'Bought' : 'Sold';
         transactionDiv.textContent = `${type} ${transaction.stockAmount} ${transaction.symbol} at price of ${transaction.pricePerStock}$ for ${transaction.totalPrice}$`;
         transactionHistoryContainer.appendChild(transactionDiv);
     });
   }
-<<<<<<< HEAD
-=======
-    // getAllTransactions(): void {
-    //   const settingsGetAllTransactions = {
-    //     url: "https://StockWizzardBackend-grateful-platypus-pd.apps.01.cf.eu01.stackit.cloud/api/transactions?email=" + this.authComponent.getCookie("email") + "&token=" + this.authComponent.getCookie("token") + "&sortby=date",
-    //     method: "GET",
-    //     headers: {
-    //       'Accept': 'application/json',
-    //       'Content-Type': 'application/json'
-    //     }
-    //   };
-      
-    //   this.http.post(settingsLogin.url, settingsLogin.body, { headers: settingsLogin.headers })
-    //     .subscribe(
-    //       (data: any) => {
-    //         // Setze die Cookies
-    //         this.authComponent.setCookie("token", data.token);
-    //         this.authComponent.setCookie("firstname", data.user.firstname);
-    //         this.authComponent.setCookie("lastname", data.user.lastname);
-    //         this.authComponent.setCookie("email", data.user.email);
-    //         alert("Login successfully");
-    //         this.router.navigate(['/content/home']); // Navigiere zur Home-Seite
-    //       },
-    //       (error) => {
-    //         if (error.status === 400 || error.status === 401 || error.status === 500) {
-    //           alert(error.error.answer);
-    //         } else {
-    //           alert("An unexpected error occurred. Status: " + error.status);
-    //         }
-    //       }
-    //     );
-    // }
->>>>>>> cc6f00765beb22d4954a5d511a79cac822f6e9af
+
+    getAllPortfolioStocks(): void {
+      const email = this.authComponent.getCookie("email"); // Hole die E-Mail aus dem Cookie
+      const token = this.authComponent.getCookie("token"); // Hole das Token aus dem Cookie
+      const sortby = "symbol"; // Beispielwert, kann angepasst werden
+
+      this.apollo
+        .watchQuery({
+          query: GET_ALL_PORTFOLIOSTOCKS,
+          variables: { email, token, sortby },
+        })
+        .valueChanges
+        .pipe(
+          tap(({ data } : any) => 
+            {
+              this.portfolioStocks = data.getAllPortfolioStocks || [];
+              this.displayPortfolioStocks(this.portfolioStocks);
+              this.portfolioStocks.forEach(portfolioStock => {
+                var positionAmount = portfolioStock.stockAmount;
+                this.getNewCurrentValue(portfolioStock.symbol, (price) => { // Arrow-Funktion
+                    let totalValue = this.roundToTwoDecimalPlaces(price * positionAmount);
+                    this.setNewCurrentValue(totalValue, portfolioStock.symbol);
+                    this.updateStockDisplay(portfolioStock.symbol, totalValue, portfolioStock.boughtValue);
+                });
+              })
+          }
+        )
+        )
+        .subscribe({
+          next: () => {
+          },
+          error: (error) => {
+            this.error = error;
+            this.loading = false;
+            alert('An unexpected error occurred: ' + error.message);
+          },
+      });
+  }
+
+  getNewCurrentValue(stockName: string, callback: (value: number) => void): void {
+    const editCurrentValueRegister = {
+        url: `https://api.polygon.io/v2/aggs/ticker/${stockName}/prev?adjusted=true&apiKey=Vf080TfqbqvnJHcpt2aP9Ec1XL21Xb0D`,
+        method: "GET",
+        dataType: 'json',
+    };
+
+    this.http.get<any[]>(editCurrentValueRegister.url).subscribe({
+        next: (data: any) => {
+            callback(this.roundToTwoDecimalPlaces(data.results[0].c));
+        },
+        error: () => { // Arrow function here
+            console.log("Error fetching stock price, retrying...");
+            setTimeout(() => {
+                this.getNewCurrentValue(stockName, callback); // Erneuter Versuch
+            }, 2000);
+        }
+    });
+  }
+
+  setNewCurrentValue(currentValue: number, symbol: string): void {
+    const email = this.authComponent.getCookie("email"); 
+    const token = this.authComponent.getCookie("token"); 
+    this.apollo.mutate({
+      mutation: EDIT_CURRENT_VALUE,
+      variables: {
+        token,
+        email,
+        symbol,
+        newValue: currentValue,
+      },
+    }).subscribe({
+      next: (response) => {
+        console.log("Antwort vom Server:", response.data);
+      },
+      error: (error) => {
+        console.error("Fehler bei der Anfrage:", error);
+        alert("Ein Fehler ist aufgetreten: " + (error.message || "Unbekannter Fehler."));
+      }
+    });
+  }
+
+  // Zeigt alle Portfolio-Aktien an
+  displayPortfolioStocks(portfolioStocks: { symbol: string }[]): void {
+    const stockListContainer = document.querySelector('.portfolio .stock-list') as HTMLElement;
+
+    stockListContainer.innerHTML = ''; 
+
+    portfolioStocks.forEach(stock => {
+        // Ruft die displayPortfolioStock-Funktion auf, um jede Aktie anzuzeigen
+        this.displayPortfolioStock(stock.symbol);
+    });
+  }
+
+  // Zeigt eine einzelne Portfolio-Aktie an
+  displayPortfolioStock(symbol: string): void {
+    const stockListContainer = document.querySelector('.portfolio .stock-list') as HTMLElement;
+
+    const stockDiv = document.createElement('div');
+    stockDiv.id = symbol; // Füge eine ID für die spätere Aktualisierung hinzu
+    stockDiv.innerHTML = `${symbol}: Calculating Portfolio data... <span class="change"></span>`;
+    
+    stockListContainer.appendChild(stockDiv);
+  }
+
+  getUserBudget(): void {
+    const email = this.authComponent.getCookie("email"); // Hole die E-Mail aus dem Cookie
+    const token = this.authComponent.getCookie("token"); // Hole das Token aus dem Cookie
+
+    this.apollo
+      .watchQuery({
+        query: GET_USER_BUDGET,
+        variables: { email, token },
+      })
+      .valueChanges
+      .pipe(
+        tap(({ data } : any) => 
+          {
+            this.budget = data.getUserProfile.budget || [];
+        }
+      )
+      )
+      .subscribe({
+        next: () => {
+          var budgetElement = document.querySelector('.remaining-budget') as HTMLElement;
+          budgetElement.textContent = this.budget + " $";
+        },
+        error: (error) => {
+          var budgetElement = document.querySelector('.remaining-budget') as HTMLElement;
+          budgetElement.textContent = JSON.parse(error.message).answer;
+        },
+    });
+  }
+  
+  getPortfolioValue(): void {
+    const email = this.authComponent.getCookie("email"); // Hole die E-Mail aus dem Cookie
+    const token = this.authComponent.getCookie("token"); // Hole das Token aus dem Cookie
+
+    this.apollo
+      .watchQuery({
+        query: GET_TOTAL_PORTFOLIO_VALUE,
+        variables: { email, token },
+      })
+      .valueChanges
+      .pipe(
+        tap(({ data } : any) => 
+          {
+            const portfolioData = data.getUserPortfolio;
+            const stockValue = this.roundToTwoDecimalPlaces(portfolioData.value);
+            this.portfolioValue = stockValue || null;
+            const { percentageChange, changeClass } = this.calculatePercentage(portfolioData.startValue, portfolioData.value);
+            const stockElement = document.querySelector('.portfolio-value') as HTMLElement;
+            stockElement.innerHTML = `${stockValue}$ <span class="percentage.${changeClass}">${percentageChange}</span>`;
+        }
+      )
+      )
+      .subscribe({
+        next: () => {
+        },
+        error: (error) => {
+          var budgetElement = document.querySelector('.portfolio-value') as HTMLElement;
+          budgetElement.textContent = JSON.parse(error.message).answer;
+        },
+    });
+  }
+
+  roundToTwoDecimalPlaces(value: number): number {
+    return Math.round(value * 100) / 100;
+  }
+
+  calculatePercentage(boughtValue: number, currentValue: number): { percentageChange: string; changeClass: string } {
+    const percentageChange = ((currentValue - boughtValue) / boughtValue * 100).toFixed(2);
+    const changeClass = parseFloat(percentageChange) >= 0 ? 'positive' : 'negative';
+    const sign = parseFloat(percentageChange) >= 0 ? '+' : '';
+
+    return {
+        percentageChange: `${sign}${percentageChange}%`,
+        changeClass: changeClass
+    };
+  }
+
+  updateStockDisplay(symbol: string, currentValue: number, boughtValue: number): void {
+    const stockElement = document.getElementById(symbol) as HTMLElement;
+
+    if (!stockElement) {
+        console.error(`Element mit dem Symbol "${symbol}" wurde nicht gefunden.`);
+        return;
+    }
+
+    const stockValue = this.roundToTwoDecimalPlaces(currentValue);
+
+    const { percentageChange, changeClass } = this.calculatePercentage(boughtValue, currentValue);
+    stockElement.innerHTML = `${symbol}: ${stockValue}$ <span class="change ${changeClass}">${percentageChange}</span>`;
+  }
+
 }
 
 const GET_ALL_TRANSACTIONS = gql`
@@ -150,4 +363,45 @@ const GET_ALL_TRANSACTIONS = gql`
       leftInPortfolio
     }
   }
+`;
+
+const GET_ALL_PORTFOLIOSTOCKS = gql`
+  query getAllPortfolioStocks($email: ID!, $token: String!, $sortby: String) {
+    getAllPortfolioStocks(email: $email, token: $token, sortby: $sortby) {
+      portfolioID
+      symbol 
+      stockAmount 
+      boughtValue 
+      currentValue 
+    }
+  }
+`;
+
+const GET_USER_BUDGET = gql`
+  query getUserProfile($email: ID!, $token: String!) {
+    getUserProfile(email: $email, token: $token) {
+      firstname
+      lastname
+      email
+      password
+      budget
+    }
+  }
+`;
+
+const GET_TOTAL_PORTFOLIO_VALUE = gql`
+  query getUserPortfolio($email: ID!, $token: String!) {
+    getUserPortfolio(email: $email, token: $token) {
+      portfolioID
+      value
+      startValue
+      email
+    }
+  }
+`;
+
+const EDIT_CURRENT_VALUE = gql`
+  mutation editCurrentValue($token: String!, $email: String!, $symbol: String!, $newValue: Float!) {
+    editCurrentValue(token: $token, email: $email, symbol: $symbol, newValue: $newValue)
+}
 `;
